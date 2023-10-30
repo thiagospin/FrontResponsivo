de6e518ced4e9d78a39685181f3b9f91c3421d1e
Thiago Pinheiro <pinheiro1980@gmail.com>
2023-10-30 17:05:56 -0300

    Erro na exibição do menu

--- scripts/screenchanges.js
+++ scripts/screenchanges.js
@@ -4,12 +4,24 @@ window.document.getElementById('closeMenu').addEventListener('click', fecharMenu
 function abrirMenu() {
 
     menu.style.display = 'flex'
-    menu.style.opacity = '1'
-    
+    menu.style.right = (menu.offsetWidth * -1) + 'px'
+    openMenu.style.display = 'none'
+
+    setTimeout(() => { 
+        menu.style.opacity = '1' 
+        menu.style.right = '0'
+    }, 10)
+
 }
 
 function fecharMenu() {
     menu.style.opacity = '0'
-    menu.removeAttribute('style')
+    menu.style.right = (menu.offsetWidth * -1) + 'px'
+
+    setTimeout(() => {
+        menu.removeAttribute('style')
+        openMenu.removeAttribute('style')
+    }, 200)
+    
 }
 
